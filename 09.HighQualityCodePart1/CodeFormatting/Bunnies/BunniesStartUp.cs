0a3f5788d2c11f8cfad6ddb5316645bf9f9e47e9
Alex Nestorov <nestorov.alexander@gmail.com>
2016-08-24 22:49:10 +0300

    update homework Code Formatting
    Add new task for homework Code Formatting

---
+++ 09.HighQualityCodePart1/CodeFormatting/Bunnies/BunniesStartUp.cs
@@ -0,0 +1,93 @@
+﻿namespace Bunnies
+{
+    using Common;
+    using Enums;
+    using Models;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class BunniesStartUp
+    {
+        public static void Main(string[] args)
+        {
+            var bunnies = new List<Bunny>
+            {
+                new Bunny
+                {
+                    Name = "Leonid",
+                    Age = 1,
+                    FurType = FurType.NotFluffy
+                },
+                new Bunny
+                {
+                    Age = 2,
+                    Name = "Rasputin",
+                    FurType = FurType.ALittleFluffy
+                },
+                new Bunny
+                {
+                    Age = 3,
+                    Name = "Tiberii",
+                    FurType = FurType.ALittleFluffy
+                },
+                new Bunny
+                {
+                    Name = "Neron",
+                    Age = 1,
+                    FurType = FurType.ALittleFluffy,
+                },
+                new Bunny
+                {
+                    Name = "Klavdii",
+                    Age = 3,
+                    FurType = FurType.Fluffy
+                },
+                new Bunny
+                {
+                    Name = "Vespasian",
+                    Age = 3,
+                    FurType = FurType.Fluffy
+                },
+                new Bunny
+                {
+                    Name = "Domician",
+                    Age = 4,
+                    FurType = FurType.FluffyToTheLimit
+                },
+                new Bunny
+                {
+                    Name = "Tit",
+                    Age = 2,
+                    FurType = FurType.FluffyToTheLimit
+                }
+            };
+        }
+
+        public void IntroduceBunnies(IList<Bunny> bunnies)
+        {
+            var consoleWriter = new ConsoleWriter();
+            foreach (var bunny in bunnies)
+            {
+                bunny.Introduce(consoleWriter);
+            }
+        }
+
+        public void CreateBunniesFromFile()
+        {
+            var bunniesFilePath = @"..\..\bunnies.txt";
+            var fileStream = File.Create(bunniesFilePath);
+            fileStream.Close();
+        }
+
+        public void SaveBunniesToFile(IList<Bunny> bunnies, string bunniesFilePath)
+        {
+            using (var streamWriter = new StreamWriter(bunniesFilePath))
+            {
+                foreach (var bunny in bunnies)
+                {
+                    streamWriter.WriteLine(bunny.ToString());
+                }
+            }
+        }
+    }
+}
