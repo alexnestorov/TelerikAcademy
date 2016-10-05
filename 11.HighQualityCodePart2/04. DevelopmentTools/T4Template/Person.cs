f6a3be22f3fdd04bc38cd421c7577388f79efb9e
Alex Nestorov <nestorov.alexander@gmail.com>
2016-10-06 00:54:38 +0300

    add development tools homework

---
+++ 11.HighQualityCodePart2/04. DevelopmentTools/T4Template/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace T4Template
+{
+    public class Person
+    {
+        public Person(string firstname, string lastname, int age, string town, string workPlace, string bankAccount, string degree)
+        {
+            this.FirstName = firstname;
+
+            this.LastName = lastname;
+
+            this.Age = age;
+
+            this.Town = town;
+
+            this.WorkPlace = workPlace;
+
+            this.BankAccount = bankAccount;
+
+            this.UniversityDegree = degree;
+
+        }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+
+        public string Town { get; private set; }
+
+        public string WorkPlace { get; private set; }
+
+        public string BankAccount { get; private set; }
+
+        public string UniversityDegree { get; private set; }
+    }
+}
---
+++ 11.HighQualityCodePart2/04. DevelopmentTools/T4Template/Program.cs
@@ -0,0 +1,19 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+
+namespace T4Template
+{
+    class Program
+    {
+        public static readonly ILog Log = LogManager.GetLogger(typeof(Program));
+        static void Main(string[] args)
+        {
+            Person person = new Person("Alexander", "Alexandrov", 25, "Sofia", "ITCompany", "DSK Klon Slavqnska beseda", "Master degree");
+            Console.WriteLine(person.FirstName);
+
+            XmlConfigurator.Configure();
+            Log.Debug(person.FirstName);
+        }
+    }
+}
